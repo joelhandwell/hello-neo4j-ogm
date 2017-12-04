b61bc3b44c05ab7d8195b46f472bf0d3f42bfd5a
Joel Handwell <joelhandwell@gmail.com>
2017-12-04 04:04:45 -0500

    Use Neo4j

---
+++ src/main/java/hello/RelTypes.java
@@ -0,0 +1,7 @@
+package hello;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum RelTypes implements RelationshipType {
+    KNOWS
+}
--- src/main/kotlin/hello/Hello.kt
+++ src/main/kotlin/hello/Hello.kt
@@ -1,5 +1,10 @@
 package hello
 
+import org.neo4j.graphdb.Direction
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.factory.GraphDatabaseFactory
+import java.io.File
+
 fun main(args : Array<String>){
 
     val greeter = Greeter()
@@ -9,5 +14,39 @@ fun main(args : Array<String>){
 }
 
 class Greeter {
-    fun greet() = "Hello World!"
+
+    private val graphDb :GraphDatabaseService = GraphDatabaseFactory().newEmbeddedDatabase(File("graph.db"))
+
+    init {
+        Runtime.getRuntime().addShutdownHook(Thread(graphDb::shutdown))
+    }
+
+    fun greet(): String?{
+
+        var greet: String? = null
+
+        graphDb.beginTx().use { tx ->
+
+            val firstNode = graphDb.createNode()
+            firstNode.setProperty("message", "Hello ")
+
+            val secondNode = graphDb.createNode()
+            secondNode.setProperty("message", "World!")
+
+            val relationship = firstNode.createRelationshipTo(secondNode, RelTypes.KNOWS)
+            relationship.setProperty("message", "with Neo4j")
+
+            greet = firstNode.getProperty("message") as String + secondNode.getProperty("message") as String
+
+            firstNode.getSingleRelationship(RelTypes.KNOWS, Direction.OUTGOING).delete()
+
+            firstNode.delete()
+            secondNode.delete()
+
+            tx.success()
+        }
+
+        return greet
+
+    }
 }
